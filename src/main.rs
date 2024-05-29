c3909c59b0b340d612d1c5eb4b8a3fed13abebbb
Parthiv Mothipati <pmothipati@gmail.com>
2024-05-28 20:55:12 -0700

    loop until break

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
-    for x in 1..5{
-        println!("x: {x}")
-    }
-    for elem in [1, 2, 3, 4, 5] {
-        println!("elem: {elem}")
+    let mut i = 0;
+    loop {
+        i += 1;
+        println!("{i}");
+        if i > 100 {
+            // Loop statement loops until a break
+            break;
+        }
     }
 }
