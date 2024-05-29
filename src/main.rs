d3c1c81eea6ce0536df2ae73a87e0278ab989fb6
Parthiv Mothipati <pmothipati@gmail.com>
2024-05-28 19:39:33 -0700

    if expressions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-fn fib(n: u32) -> u32 {
-    return if n < 2 {
-        n
-    } else {
-        fib(n - 1) + fib(n - 2)
-    }
-}
-
 fn main() {
-    let n = 20;
-    println!("fib({n}) = {}", fib(n));
+    let x = 10;
+    if x == 0 {
+        println!("X is equal to zero")
+    } else if x < 100 {
+        println!("X is a reasonably sized number")
+    } else {
+        println!("X is a huge number")
+    }
+
+    let size = if x < 20 { "small" } else { "large" };
+    println!("number size: {}", size)
 }
