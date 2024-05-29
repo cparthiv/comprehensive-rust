ef36408c7a5e5d5bdd93cfe435ef2b8858e5669f
Parthiv Mothipati <pmothipati@gmail.com>
2024-05-28 21:56:55 -0700

    Revert "break out of nested loops with label"
    This reverts commit 5d45e6392f6f751545b3e0c87207b6e4d3a9cc2c.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let z = 13;
-    // Block
-    // Each block has a value and a type which are those of the last expression of the block
-    let x = {
-        let y = 10;
-        println!("y: {y}");
-        z - y
-    };
-    println!("x: {x}");
+    let mut i = 0;
+    loop {
+        i += 1;
+        // Exit out of loop
+        if i > 5 { break; }
+        // Start next iteration immediately
+        if i % 2 == 0 { continue; }
+        println!("{}", i)
+    }
 }
