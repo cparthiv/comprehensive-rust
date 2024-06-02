e7df2c008dff3d5a7490c7c8a83e245cd89c0afe
Parthiv Mothipati <pmothipati@gmail.com>
2024-06-02 12:56:25 -0700

    destructuring tuple

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
-    let primes = [2, 3, 5, 7, 11, 13, 17, 19];
-    for prime in primes {
-        for i in 2..prime {
-            assert_ne!(prime % i, 0)
-        }
-    }
+    print_tuple((1, 3))
+}
+
+fn print_tuple(tuple: (i32, i32)) {
+    // Extracting variables (2 lines)
+    // let left = tuple.0;
+    // let right = tuple.1;
+
+    // Destructuring tuple (1 line)
+    let (left, right) = tuple;
+    println!("left: {left}, right: {right}");
 }
