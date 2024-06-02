2ecab27dd43500c8cf192433056e364637d80719
Parthiv Mothipati <pmothipati@gmail.com>
2024-06-02 11:13:43 -0700

    Tuples

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut a: [i8; 10] = [42; 10];
-    a[5] = 0;
-    println!("a: {a:?}")
+    let t: (i8, bool) = (7, true);
+    println!("t.0: {}", t.0);
+    println!("t.1: {}", t.1);
 }
